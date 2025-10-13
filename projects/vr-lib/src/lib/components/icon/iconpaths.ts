380f63988fcb7e548cd8c4c7056cc69f47003ab2
Ilze Dombrovska <ilze.dombrovska@vrsverige.com>
2025-10-13 15:44:32 +0300

    yelowvest icons

--- projects/vr-lib/src/lib/components/icon/iconpaths.ts
+++ projects/vr-lib/src/lib/components/icon/iconpaths.ts
@@ -1,3 +1,7 @@
 export const iconPaths: string[] = [
   '/assets/icons/ico_no_data.svg',
+  '/assets/icons/ico_yellowvest_coordinator.svg',
+  '/assets/icons/ico_yellowvest_participant.svg',
+  '/assets/icons/ico_yellowvest_white.svg',
+  '/assets/icons/ico_yellowvest.svg',
 ];
